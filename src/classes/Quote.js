e259b48d55623f3a913cb96d1c8cfbaca5632443
max <max.prokopchuk@gmail.com>
2025-03-23 20:23:40 +0100

    initial commit: add random quotes application with HTML, CSS, and JavaScript

---
+++ src/classes/Quote.js
@@ -0,0 +1,12 @@
+class Quote {
+  constructor(id, text, author) {
+    this.id = id;
+    this.text = text;
+    this.author = author;
+    this.isFavorited = false;
+  }
+
+ 
+}
+
+export default Quote;
---
+++ src/utils/MathUtils.js
@@ -0,0 +1,10 @@
+
+
+
+class MathUtils {
+  static generateRandomInt(maxInt) {
+    return Math.floor(Math.random() * maxInt);
+  }
+}
+
+export default MathUtils;
