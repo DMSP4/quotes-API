7c2c884905099c30de27e657eed797d9fb5a4054
max <max.prokopchuk@gmail.com>
2025-04-05 12:19:14 +0200

    feat: add configuration file for API URLs and update quote fetching methods to use config

---
+++ client/src/config.js
@@ -0,0 +1,8 @@
+
+const config = {
+  PUBLIC_API_URL: "https://quoteslate.vercel.app/api/quotes/random",
+  API_URL: "http://localhost:3000",
+};
+
+export default config;
+// This file contains the configuration for the API URLs used in the application.
